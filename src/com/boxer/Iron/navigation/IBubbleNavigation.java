58c2d1bf10ff60b5f004e86ce023d04cdbf67248
Alex <du.alexcruz@gmail.com>
2021-05-28 08:30:08 +0000

    Initial IRON commit
    Based on ShapeshiftOS Shapeshifter, AquariOS CoralReef, Corvus RavenLair, AOSiP OwlsNest and DU Dirty-Tweaks
    
    Co-authored-by: Calebcabob <calphonic@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: Akhil Narang <akhil@akhilnarang.me>
    Co-authored-by: gaurav <19gauravk95@gmail.com>
    Co-authored-by: Genkzsz11 <Genkzsz11@gmail.com>
    Co-authored-by: Alexander Dadukin <hi@st235.xyz>
    Co-authored-by: Ralf979 <ralf979@gmail.com>

---
+++ src/com/boxer/Iron/navigation/IBubbleNavigation.java
@@ -0,0 +1,15 @@
+package com.boxer.Iron.navigation;
+
+import android.graphics.Typeface;
+import com.boxer.Iron.navigation.BubbleNavigationChangeListener;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+}
